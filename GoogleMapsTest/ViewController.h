9b9e6f0800d77089af2bd3d75d839e267271943c
Vladislav <vladislav@MacBook-Pro-Vladislav.local>
2015-04-21 20:15:49 +0400

    Initial Commit

---
+++ GoogleMapsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleMapsTest
+//
+//  Created by Vladislav on 21.04.15.
+//  Copyright (c) 2015 Vladislav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
